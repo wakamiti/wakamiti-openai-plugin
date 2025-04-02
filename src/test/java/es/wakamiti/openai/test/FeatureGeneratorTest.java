b54b8b3723f9f92b831ed49de4501ed82b9ec02e
Maria Galbis <mgalbis@iti.es>
2025-04-02 11:14:59 +0200

    test: Add feature generator tests based on different swagger versions

--- src/test/java/es/wakamiti/openai/test/FeatureGeneratorTest.java
+++ src/test/java/es/wakamiti/openai/test/FeatureGeneratorTest.java
@@ -3,20 +3,20 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-package es.wakamiti.chatgpt.test;
+package es.wakamiti.openai.test;
 
 
 import com.openai.client.okhttp.OpenAIOkHttpClient;
-import es.wakamiti.api.lang.WakamitiException;
-import es.wakamiti.chatgpt.FeatureGenerator;
+import es.wakamiti.openai.FeatureGenerator;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.assertj.core.util.Files;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.FieldSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.mockserver.integration.ClientAndServer;
 import org.mockserver.matchers.Times;
 import org.mockserver.model.HttpRequest;
@@ -25,21 +25,16 @@ import org.mockserver.model.MediaType;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.text.MessageFormat;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import java.lang.reflect.Field;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockserver.integration.ClientAndServer.startClientAndServer;
@@ -64,29 +59,15 @@ public class FeatureGeneratorTest {
             .map(f -> TEMP_PATH.resolve(f + ".feature").toFile())
             .toList();
 
-    private static Stream<Arguments> schemas() {
-        return Stream.of(
-                Arguments.of("examples/v2.0/json/petstore.json"),
-                Arguments.of("examples/v2.0/json/petstore-separate/spec/swagger.json"),
-                Arguments.of("examples/v2.0/yaml/petstore.yaml"),
-                Arguments.of("examples/v2.0/yaml/petstore-separate/spec/swagger.yaml"),
-                Arguments.of("examples/v3.0/petstore.json"),
-                Arguments.of("examples/v3.0/petstore.yaml")
-        );
-    }
-
-    private static Stream<Arguments> features() {
-        return Stream.of(
-                Arguments.of("pets1/getPets", "getPets", "pets1"),
-                Arguments.of("pets1/postPets", "postPets", "pets1"),
-                Arguments.of("pets2/getPetsById", "getPetsById", "pets2"),
-                Arguments.of("deletePetsById", "deletePetsById", null)
-        );
-    }
-
     @AfterAll
     public static void tearDown() {
         server.close();
+    }
+
+    private static <T> T getField(Object o, String field, Class<T> cls) throws NoSuchFieldException, IllegalAccessException {
+        Field buildField = o.getClass().getDeclaredField(field);
+        buildField.setAccessible(true);
+        return cls.cast(buildField.get(o));
     }
 
     @BeforeEach
@@ -157,21 +138,20 @@ public class FeatureGeneratorTest {
 
     @DisplayName("Test schema by http url")
     @Test
-//    @ParameterizedTest(name = "Feature ''{0}'' ")
-//    @MethodSource("features")
     public void testGenerateTestWhenHttpURLWithSuccess()
             throws IOException, NoSuchFieldException, IllegalAccessException {
         // Prepare
         for (File file : FEATURES) {
-            Matcher matcher = Pattern.compile(Pattern.quote(TEMP_PATH.toString()) + "([/\\\\]([^/\\\\]++))?[/\\\\]([^/\\\\]+?)\\.feature$")
+            Matcher matcher = Pattern.compile(
+                            Pattern.quote(TEMP_PATH.toString()) + "([/\\\\]([^/\\\\]++))?[/\\\\]([^/\\\\]+?)\\.feature$")
                     .matcher(file.toString());
             matcher.find();
             String apiId = matcher.group(2);
             String operationId = matcher.group(3);
 
             String r = ".+schema=\\{.+}\\\\nlanguage=en";
-            if (apiId != null) r += "\\\\napiId="+apiId;
-            r += "\\\\noperationId="+operationId+".+";
+            if (apiId != null) r += "\\\\napiId=" + apiId;
+            r += "\\\\noperationId=" + operationId + ".+";
             mockServer(
                     request()
                             .withMethod("POST")
@@ -222,12 +202,6 @@ public class FeatureGeneratorTest {
 
     private void mockServer(HttpRequest expected, HttpResponse response, Times times) {
         server.when(expected, times).respond(response);
-    }
-
-    private static <T> T getField(Object o, String field, Class<T> cls) throws NoSuchFieldException, IllegalAccessException {
-        Field buildField = o.getClass().getDeclaredField(field);
-        buildField.setAccessible(true);
-        return cls.cast(buildField.get(o));
     }
 
 }
